c588bb2d2694743bc409eb5a065b51292bbbbe43
CptSpooky <legacyofthedonut@gmail.com>
2021-01-02 22:26:37 -0500

    another logo fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,6 @@ import About from './About';
 import Skills from './Skills';
 import Portfolio from './Portfolio';
 import Contact from './Contact';
-import Logo from "../assets/logo.svg";
 
 class SideNav extends Component {
   state = {
@@ -24,7 +23,7 @@ render() {
     return (
       <Router>
         <MDBContainer className="mobileNav" style={{ margin: "0px", width: "100%" }}>
-          <img alt="logo" src={Logo} className="rounded-circle" style={{ maxWidth: "50px"}} />
+          <img alt="logo" src="./assets/logo.svg" className="rounded-circle" style={{ maxWidth: "50px"}} />
           <MDBView hover onClick={this.sidenavToggle("Left")}>
             <MDBIcon className="hamburgerIcon" size="lg" icon="th" />
             <MDBMask overlay="indigo-light" />
